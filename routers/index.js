0deb2221f45b07b21a6e5831e703a0a10b40b6eb
m-reza1 <mrejaa@gmail.com>
2025-02-06 20:32:22 +0700

    register,login,homepage

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router =  require('express').Router()
 const userController = require('../controllers/userController')
 const adminController = require('../controllers/adminController')
+const accountAuth = require('../middleware/auth')
 
 // REGISTER
 router.get("/register", userController.registerForm)
@@ -9,24 +10,25 @@ router.post("/register", userController.saveRegisterForm)
 
 // LOGIN
 router.get("/login", userController.loginPage)
-router.post("/login", userController.loginPage)
-
+router.post("/login", userController.loggedIn)
 
 // USER
 router.get('/', userController.home) //home
-router.get('/invoice', userController.invoice) // invoice
-router.get('/cart', userController.cart) // invoice
-// router.get('/menu',userController.menu)// show all item
+router.get('/menu',userController.menu)// show all item
+
+router.use(accountAuth)
+
+router.get('/cart',accountAuth, userController.cart)
 // router.get('/invoice', userController.invoice) // invoice
 // router.get('/profile')
 
 // ADMIN
-router.get('/', adminController.X);
-router.get('/menu', adminController.getMenu);
-router.get('/profile', adminController.showProfile);
-router.get('/', adminController.X);
-router.get('/', adminController.X);
-router.get('/', adminController.X);
-router.get('/', adminController.X);
+// router.get('/', adminController.X);
+// router.get('/menu', adminController.getMenu);
+// router.get('/profile', adminController.showProfile);
+// router.get('/', adminController.X);
+// router.get('/', adminController.X);
+// router.get('/', adminController.X);
+// router.get('/', adminController.X);
 
 module.exports = router
